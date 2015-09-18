5359cd083407b89c4f7a9240b852d744b0384bb9
Dominique Broeglin <dominique.broeglin@gmail.com>
2015-09-18 18:45:59 +0200

    First working tracing bullet

--- src/main/java/fr/broeglin/integration/iti/routes/ItiRouteBuilder.java
+++ src/main/java/fr/broeglin/integration/iti/routes/ItiRouteBuilder.java
@@ -9,7 +9,6 @@ import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.SimpleBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
-import org.springframework.util.StringUtils;
 
 import sbmappservices72.Auth;
 import sbmappservices72.FieldIdentifier;
@@ -19,136 +18,163 @@ import sbmappservices72.TableIdentifier;
 
 public class ItiRouteBuilder extends RouteBuilder {
 
-  private static final String OPTIONS = "OPTIONS";
+	@Override
+	public void configure() throws Exception {
+		onException(Exception.class)
+				.handled(true)
+				.to("log:error?showAll=true&level=ERROR")
+				.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+				.setBody(constant("{ \"code\": 000 }"));
 
-  @Override
-  public void configure() throws Exception {
-    onException(Exception.class)
-        .handled(true)
-        .to("log:error?showAll=true&level=ERROR")
-        .setBody(constant("{ \"code\": 000 }"));
+		from("cxfrs:bean:rsServer?bindingStyle=SimpleConsumer")
+				.to("log:request?showHeaders=true")
+				.setHeader(CxfConstants.OPERATION_NAME,
+						constant("GetItemsByQuery"))
+				.setProperty(
+						AUTH,
+						createAuth(simple("{{iti.backend.username}}"),
+								simple("{{iti.backend.password}}")))
+				.setProperty(TABLE,
+						createTableIdentifier(simple("{{iti.cis.tableName}}")))
+				.setProperty(
+						WHERE_CLAUSE,
+						simpleFromProperty(simple("iti.cis.${header.ciType}.where")))
+				.setProperty(
+						ORDER_BY_CLAUSE,
+						simpleFromProperty(simple("iti.cis.${header.ciType}.orderBy")))
+				.setProperty(
+						OPTIONS,
+						createMultipleOptions(simpleFromProperty(simple("iti.cis.${header.ciType}.fields"))))
+				.setBody(createRequest())
+				.to("{{iti.cis.endPoint}}")
+				.to("log:responseProcessing?showHeaders=true")
+				// .process(new Processor() {
+				// @Override
+				// public void process(Exchange ex) throws Exception {
+				// ObjectMapper mapper = new ObjectMapper();
+				// ObjectNode node = mapper.createObjectNode();
+				//
+				// node.put("_type", "coucou");
+				//
+				// ex.getIn().setBody(node);
+				// }
+				// })
+				.removeHeaders("*")
+				.to("log:output")
+				.marshal("json");
+	}
 
-    from("cxfrs:bean:rsServer?bindingStyle=SimpleConsumer")
-        .to("log:request?showHeaders=true")
-        .setHeader(CxfConstants.OPERATION_NAME, constant("GetItemsByQuery"))
-        .setProperty(AUTH,
-            createAuth(simple("{{iti.backend.username}}"), simple("{{iti.backend.password}}")))
-        .setProperty(WHERE_CLAUSE,
-            simpleFromProperty(simple("iti.cis.${header.ciType}.where")))
-        .setProperty(ORDER_BY_CLAUSE,
-            simpleFromProperty(simple("iti.cis.${header.ciType}.orderBy")))
-        .setProperty(OPTIONS,
-            createMultipleOptions(simpleFromProperty(simple("iti.cis.${header.ciType}.where"))))
-        .setBody(createRequest())
-        .to("{{iti.cis.endPoint}}")
-        .to("log:responseProcessing?showHeaders=true")
-        // .process(new Processor() {
-        // @Override
-        // public void process(Exchange ex) throws Exception {
-        // ObjectMapper mapper = new ObjectMapper();
-        // ObjectNode node = mapper.createObjectNode();
-        //
-        // node.put("_type", "coucou");
-        //
-        // ex.getIn().setBody(node);
-        // }
-        // })
-        .removeHeaders("*")
-        .to("log:output")
-        .marshal("json");
-  }
+	// Plumbing
 
-  // Plumbing
+	private static final String AUTH = "AUTH";
+	private static final String ORDER_BY_CLAUSE = "ORDER_BY_CLAUSE";
+	private static final String WHERE_CLAUSE = "WHERE_CLAUSE";
+	private static final String TABLE = "TABLE";
+	private static final String OPTIONS = "OPTIONS";
+	ObjectFactory factory = new ObjectFactory();
 
-  private static final String AUTH = "AUTH";
-  private static final String ORDER_BY_CLAUSE = "ORDER_BY_CLAUSE";
-  private static final String WHERE_CLAUSE = "WHERE_CLAUSE";
-  ObjectFactory factory = new ObjectFactory();
+	private Expression createTableIdentifier(final Expression tableNameExp) {
+		return new Expression() {
 
-  private TableIdentifier createTableIdentifier(String dbName) {
-    TableIdentifier id = factory.createTableIdentifier();
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T evaluate(Exchange exchange, Class<T> type) {
+				TableIdentifier id = factory.createTableIdentifier();
+				String tableName = tableNameExp
+						.evaluate(exchange, String.class);
 
-    id.setDbName(factory.createTableIdentifierDbName(dbName));
+				id.setDbName(factory.createTableIdentifierDbName(tableName));
 
-    return id;
-  }
+				return (T) id;
+			}
 
-  private Expression createMultipleOptions(Expression exp) {
+		};
 
-    return new Expression() {
+	}
 
-      @SuppressWarnings("unchecked")
-      @Override
-      public <T> T evaluate(Exchange exchange, Class<T> type) {
-        MultipleResponseItemOptions options = factory.createMultipleResponseItemOptions();
-        String value = exp.evaluate(exchange, String.class);
+	private Expression createMultipleOptions(Expression exp) {
 
-        
-        if (value != null) {
-          for (String fieldName : value.split(",")) {
-            options.getLimitedField().add(createFieldIdentifier(fieldName));
-          }
-        }
-        return (T) options;
-      }
-    };
-  }
+		return new Expression() {
 
-  private FieldIdentifier createFieldIdentifier(String name) {
-    FieldIdentifier fieldId = factory.createFieldIdentifier();
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T evaluate(Exchange exchange, Class<T> type) {
+				MultipleResponseItemOptions options = factory
+						.createMultipleResponseItemOptions();
+				String value = exp.evaluate(exchange, String.class);
 
-    fieldId.setDbName(factory.createFieldIdentifierDbName(name));
+				if (value != null) {
+					for (String fieldName : value.split(",")) {
+						options.getLimitedField().add(
+								createFieldIdentifier(fieldName));
+					}
+				}
+				return (T) options;
+			}
+		};
+	}
 
-    return fieldId;
-  }
+	private FieldIdentifier createFieldIdentifier(String name) {
+		FieldIdentifier fieldId = factory.createFieldIdentifier();
 
-  private Expression createAuth(final Expression usernameExp, final Expression passwordExp) {
+		fieldId.setDbName(factory.createFieldIdentifierDbName(name));
 
-    return new Expression() {
+		return fieldId;
+	}
 
-      @SuppressWarnings("unchecked")
-      @Override
-      public <T> T evaluate(Exchange exchange, Class<T> type) {
-        Auth auth = new Auth();
-        String username = usernameExp.evaluate(exchange, String.class);
-        String password = passwordExp.evaluate(exchange, String.class);
+	private Expression createAuth(final Expression usernameExp,
+			final Expression passwordExp) {
 
-        auth.setUserId(factory.createAuthUserId(username));
-        auth.setPassword(factory.createAuthPassword(password));
-        return (T) auth;
-      }
-    };
-  }
+		return new Expression() {
 
-  public Expression createRequest() {
-    return new Expression() {
-      @SuppressWarnings("unchecked")
-      @Override
-      public <T> T evaluate(Exchange exchange, Class<T> type) {
-        Message msg = exchange.getIn();
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T evaluate(Exchange exchange, Class<T> type) {
+				Auth auth = new Auth();
+				String username = usernameExp.evaluate(exchange, String.class);
+				String password = passwordExp.evaluate(exchange, String.class);
 
-        return (T) Arrays.asList(
-            exchange.getProperty(AUTH),
-            createTableIdentifier("DBNAME"),
-            exchange.getProperty(WHERE_CLAUSE),
-            exchange.getProperty(ORDER_BY_CLAUSE),
-            BigInteger.valueOf(msg.getHeader("offset", Long.class)),
-            BigInteger.valueOf(msg.getHeader("limit", Long.class)),
-            exchange.getProperty(OPTIONS));
-      }
-    };
-  }
+				auth.setUserId(factory.createAuthUserId(username));
+				auth.setPassword(factory.createAuthPassword(password));
+				return (T) auth;
+			}
+		};
+	}
 
-  private Expression simpleFromProperty(Expression propertyExp) {
-    return new Expression() {
+	public Expression createRequest() {
+		return new Expression() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T evaluate(Exchange exchange, Class<T> type) {
+				Message msg = exchange.getIn();
 
-      @SuppressWarnings("unchecked")
-      @Override
-      public <T> T evaluate(Exchange exchange, Class<T> type) {
-        String template = "{{" + propertyExp.evaluate(exchange, String.class) + "}}";
+				return (T) Arrays
+						.asList(
+								exchange.getProperty(AUTH),
+								exchange.getProperty(TABLE),
+								exchange.getProperty(WHERE_CLAUSE),
+								exchange.getProperty(ORDER_BY_CLAUSE),
+								BigInteger.valueOf(msg.getHeader("offset",
+										Long.class)),
+								BigInteger.valueOf(msg.getHeader("limit",
+										Long.class)),
+								exchange.getProperty(OPTIONS));
+			}
+		};
+	}
 
-        return (T) SimpleBuilder.simple(template).evaluate(exchange, String.class);
-      }
-    };
-  }
+	private Expression simpleFromProperty(Expression propertyExp) {
+		return new Expression() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T evaluate(Exchange exchange, Class<T> type) {
+				String template = "{{"
+						+ propertyExp.evaluate(exchange, String.class) + "}}";
+
+				return (T) SimpleBuilder.simple(template).evaluate(exchange,
+						String.class);
+			}
+		};
+	}
 }
